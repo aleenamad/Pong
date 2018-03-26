0bcfeda90eb6ec13ac71ff7db5b8fa4d7c8cdd27
AleenaMad <aleena.madni@gmail.com>
2018-03-26 14:10:55 -0500

    ball has animation, can bounce

--- sketch.js
+++ sketch.js
@@ -29,13 +29,44 @@ const canvas = document.getElementById('pong');
 const context = canvas.getContext('2d');
 
 const ball = new Ball;
-console.log(ball);
+ball.pos.x = 100;
+ball.pos.y = 50;
+
+ball.vel.x = 100;
+ball.vel.y = 100;
+
+let lastTime;
+function callback(millis) {
+  if (lastTime) {
+    update((millis - lastTime) / 1000);
+  }
+  lastTime = millis;
+  requestAnimationFrame(callback);
+}
+
+//// animating the Ball
+function update(dt) {
+  ball.pos.x += ball.vel.x * dt;
+  ball.pos.y += ball.vel.y * dt;
+
+if (ball.pos.x < 0 || ball.pos.x > canvas.width) {
+  ball.vel.x = -ball.vel.x;
+}
+
+if (ball.pos.y < 0 || ball.pos.y > canvas.height) {
+  ball.vel.y = -ball.vel.y;
+}
 
 //////rectangle background
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = '#000';
+  context.fillRect(0, 0, canvas.width, canvas.height);
 
 
-////// ball
-context.fillStyle = '#fff';
-context.fillRect(0, 0, 10, 10);
+  ////// ball
+  context.fillStyle = '#fff';
+  context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
+
+
+}
+
+callback();
